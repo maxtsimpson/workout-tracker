e76d2df1379874faf674ee6d6e253edc401f9278
Max Simpson <maxsimpson95@gmail.com>
2020-06-11 14:42:02 +0800

    got the create workout api route working

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -1,4 +1,5 @@
-let db = require("../models");
+const db = require("../models");
+const mongoose = require("mongoose")
 
 module.exports = function (app) {
 
@@ -12,31 +13,33 @@ module.exports = function (app) {
 
     app.get("/api/workouts/range", (req, res) => {
         const days = req.body.numberOfDays || 7
-        const startDate = new Date(Date.now()).getDate()-days
-        console.log(new Date(startDate).toDateString())
-        console.log({startDate})
-        db.Workout.find({day: {$lte: startDate }})
+        const startDate = new Date().setDate(new Date().getDate() - days)
+        console.log({ startDate })
+        db.Workout.find({ day: { $gte: startDate } })
             .then((workouts) => {
-                console.log({workouts})
+                console.log({ workouts })
                 res.json(workouts)
             })
             .catch(error => res.json(error))
     })
 
-    app.put("/api/workouts", (req, res) => {
-        db.Workout.find({})
-            .then((workouts) => {
-                console.log(workouts)
-                res.json(workouts)
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(`getting workout with id: ${req.params.id}`)
+        db.Workout.findOneAndUpdate(req.params.id, { $set: req.body })
+            .then((results) => {
+                console.log("successfully updated")
+                console.log(results)
+                res.json(results)
             })
             .catch(error => res.json(error))
     })
 
     app.post("/api/workouts", (req, res) => {
-        db.Workout.Create(req.body)
-            .then((workouts) => {
-                console.log(workouts)
-                res.json(workouts)
+        const workout = new db.Workout(req.body)
+        workout.save()
+            .then((results) => {
+                console.log(results)
+                res.json(results)
             })
             .catch(error => res.json(error))
     })
