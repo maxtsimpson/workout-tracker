81f5afa6678845feb15d43bc37d972b8291f04a8
Max Simpson <maxsimpson95@gmail.com>
2020-06-11 14:41:13 +0800

    set mongoose to debug mode to see db interaction

--- Develop/config/mongo.js
+++ Develop/config/mongo.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI
+mongoose.set('debug', true);
 
 module.exports = mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
