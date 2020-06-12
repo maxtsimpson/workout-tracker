547452c9f608c183daf8d005107e87f4613c66f9
Max Simpson <maxsimpson95@gmail.com>
2020-06-12 20:56:12 +0800

    changed seed insert to use the mongoose method for validation

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -139,9 +139,10 @@ let workoutSeed = [
 ];
 
 db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(() => db.Workout.insertMany(workoutSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    // console.log(data.result.n + " records inserted!");
+    console.log({data})
     process.exit(0);
   })
   .catch(err => {
