9819d2322c448309f047ff7c8792f868be0f2ca3
Max Simpson <maxsimpson95@gmail.com>
2020-06-15 14:40:16 +0800

    removed console logs

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -16,19 +16,15 @@ module.exports = function (app) {
         const startDate = new Date().setDate(new Date().getDate() - days)
         db.Workout.find({ day: { $gte: startDate } }).sort({ day: -1 })
             .then((workouts) => {
-                console.log({ workouts })
                 res.json(workouts)
             })
             .catch(error => res.json(error))
     })
 
     app.put("/api/workouts/:id", (req, res) => {
-        console.log(`getting workout with id: ${req.params.id}`)
-        console.log(req.body)
+
         db.Workout.findOneAndUpdate(req.params.id, { $push: { exercises: req.body} }, {new: true})
             .then((newWorkout) => {
-                console.log("updated")
-                console.log(newWorkout)
                 res.json(newWorkout)
             })
             .catch(error => res.json(error))
@@ -38,7 +34,6 @@ module.exports = function (app) {
         const workout = new db.Workout()
         workout.save()
             .then((results) => {
-                console.log(results)
                 res.json(results)
             })
             .catch(error => res.json(error))
