12ef34fdc0f5634a2e98f9d9645adf687cdcf147
Max Simpson <maxsimpson95@gmail.com>
2020-06-16 05:58:07 +0800

    removed unnecesarry commented code from connection file

--- Develop/config/mongo.js
+++ Develop/config/mongo.js
@@ -8,25 +8,3 @@ module.exports = mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 })
-
-// module.exports = {
-
-//     connect : function () { 
-//         Promise((resolve, reject) => {
-//             mongoose.connect(process.env.MONGO_URI, {
-//                 useNewUrlParser: true,
-//                 useFindAndModify: false
-//             })
-
-
-
-//             mongoClient.connect((err, client) => {
-//                 if (err) {
-//                     reject(err)
-//                 } else {
-//                     resolve(client.db('eat_da_burger').collection('burgers'))
-//                 }
-//             })
-//         })
-//     }
-// }
