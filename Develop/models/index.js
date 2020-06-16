665359b1c373d16d4f4082b54e3bf59da7b07929
Max Simpson <maxsimpson95@gmail.com>
2020-06-16 14:41:12 +0800

    models index modified to use path so can support win or linux

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -9,7 +9,7 @@ fs
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
-        const model = require(`.\\${file}`)
+        const model = require(path.join(__dirname,file))
         const modelName = path.basename(file,'.js')[0].toUpperCase() + path.basename(file,'.js').slice(1)
         db[modelName] = model;
     });
