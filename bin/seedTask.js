1f77ad1fefbeb0a3657653320d29696bc2a4d2b7
Max Simpson <maxsimpson95@gmail.com>
2020-06-18 05:43:18 +0800

    creating a heroku task to run seed every day

---
+++ bin/seedTask.js
@@ -0,0 +1,7 @@
+#! /app/bin/node
+
+//this is a heroku task that executes the seed task daily
+//so there is always up to date seed data in the app. 
+//it will also wipe any user data that is done over a day
+//but it can be disabled if the site went live and the demo was done
+require("../seeders/seed")
